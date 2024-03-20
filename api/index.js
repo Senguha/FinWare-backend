a322187c2bded9e6fe039aa3cb366d9d4255bbec
Mtseng <darychev2013@yandex.ru>
2024-03-20 16:08:38 +0500

    a

--- api/index.js
+++ api/index.js
@@ -1,3 +1,45 @@
-const app = require('../src/app')
+const express = require('express')
+const db = require('../src/dbconnection')
+const cors = require("cors");
+const corsOptions = {
+    origin: "http://localhost:5173",
+};
+const app = express()
+app.use(express.json())
+app.use(cors(corsOptions));
+
+app.get("/", (req, res) => res.send("Express on Vercel"));
+
+app.get('/companies', async (req, res) => {
+    try {
+        const data = await db.query('SELECT * FROM companies')
+        res.status(200).send(data.rows)
+    } catch (err) {
+        console.log(err)
+        res.sendStatus(500)
+    }
+})
+
+app.get('/parametres', async (req, res) => {
+    try {
+        const data = await db.query('SELECT * FROM parametres')
+        res.status(200).send(data.rows)
+    } catch (err) {
+        console.log(err)
+        res.sendStatus(500)
+    }
+})
+app.get('/contactPersons', async (req, res) => {
+    try {
+        const data = await db.query('SELECT * FROM contact_persons')
+        res.status(200).send(data.rows)
+    } catch (err) {
+        console.log(err)
+        res.sendStatus(500)
+    }
+})
+
+
+app.listen(5000, () => console.log(`Server has started on port: 5000`))
 
 module.exports = app;
